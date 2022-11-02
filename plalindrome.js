c11e433544f69391e1bddc70da801192ef261090
Musharraffaijaz <49266438+Musharraffaijaz@users.noreply.github.com>
2022-11-02 11:48:36 +0530

    First Workshop Project

---
+++ plalindrome.js
@@ -0,0 +1,17 @@
+function checkisPalindrome(str){
+    let j = str.length - 1;
+    for(let i = 0; i < j/2 ; i++){
+        let x = str[i];
+        let y = str[j-i]
+        if(x!=y){
+            return false
+        }
+        else{
+            return true
+        }
+    }
+
+}
+
+console.log(checkisPalindrome('num'))
+
